6ba2b4a08de77a224bc28fda308fa72d93f7450f
Kavinkumar-QA <kavin@zentelai.com>
2025-05-28 18:11:37 +0530

    testdatafiles

--- tests/evj.spec.js
+++ tests/evj.spec.js
@@ -1,15 +1,22 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import { selectors } from './locators.js';
+import { credentials } from './testData.js';
 
-test('Navigate, take screenshot, assert URL, click link', async ({ page }) => {
+test('Login and navigate to dashboard', async ({ page }) => {
   await page.goto('https://omega.dev.mycoveragehub.com/');
-  await page.click("(//button[text()='Sign in'])[1]");
-  await page.fill("//input[@class='input c2053dbb5 c10f8401f']","evjtest6@zentelai.com");
-  await page.fill("//input[@class='input c2053dbb5 cb1683880']","Warranty@123");
-  await page.click("//button[@type='submit']")
+
+  await page.click(selectors.signInButton);
+  await page.fill(selectors.emailInput, credentials.email);
+  await page.fill(selectors.passwordInput, credentials.password);
+  await page.click(selectors.submitButton);
+
   await page.screenshot({ path: 'screenshots/homepage.png', fullPage: true });
+
   await expect(page).toHaveURL('https://omega.dev.mycoveragehub.com/dashboard');
-  await page.click("(//div[@class='bg-white border-6 border-solid border-[#cbd0dc] rounded-[20px] p-4'])[1]");
+
+  await page.click(selectors.dashboardCard);
   await page.waitForLoadState('networkidle');
+
   console.log('New URL:', page.url());
 });
---
+++ tests/testData.js
@@ -0,0 +1,5 @@
+// testData.js
+export const credentials = {
+  email: "evjtest6@zentelai.com",
+  password: "Warranty@123"
+};
